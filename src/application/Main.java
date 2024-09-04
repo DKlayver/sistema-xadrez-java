7ba65db656860bed370423e915a1e384fe7211cb
DKlayver <92654447+DKlayver@users.noreply.github.com>
2024-09-04 10:50:45 -0300

    Camada de Xadrez e Impressao Tabuleiro

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,10 +2,13 @@ package application;
 
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class Main {
     public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro(8,8);
-        
+
+        PartidaXadrez partidaXadrez = new PartidaXadrez();
+        UI.printTabuleiro(partidaXadrez.getPecas());
     }
 }
--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -10,4 +10,28 @@ public class Tabuleiro {
         this.colunas = colunas;
         pecas = new Peca[linhas][colunas];
     }
+
+    public int getLinhas() {
+        return linhas;
+    }
+
+    public void setLinhas(int linhas) {
+        this.linhas = linhas;
+    }
+
+    public int getColunas() {
+        return colunas;
+    }
+
+    public void setColunas(int colunas) {
+        this.colunas = colunas;
+    }
+
+    public Peca piece(int linha, int coluna) {
+        return pecas[linha][coluna];
+    }
+
+    public Peca peca(Posicao posicao){
+        return pecas[posicao.getLinha()][posicao.getColuna()];
+    }
 }
